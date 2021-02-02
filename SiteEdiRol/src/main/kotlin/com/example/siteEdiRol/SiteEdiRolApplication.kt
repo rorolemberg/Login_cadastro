01b13ed3646376be3ca143e3503cf255b30d6f9c
Rodrigo <rodrigo_rolemberg@hotmail.com>
2021-02-02 20:16:23 -0300

    Primeito Commit

---
+++ SiteEdiRol/src/main/kotlin/com/example/siteEdiRol/SiteEdiRolApplication.kt
@@ -0,0 +1,11 @@
+package com.example.siteEdiRol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiteEdiRolApplication
+
+fun main(args: Array<String>) {
+    runApplication<SiteEdiRolApplication>(*args)
+}
---
+++ SiteEdiRol/src/test/kotlin/com/example/siteEdiRol/SiteEdiRolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.siteEdiRol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SiteEdiRolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

19cc30e76351c69e3dc6b3ccd8e320859b209476
Rodrigo <rodrigo_rolemberg@hotmail.com>
2021-02-02 21:02:29 -0300

    Criando Controller

--- SiteEdiRol/src/main/kotlin/com/example/siteEdiRol/Controller.kt
+++ SiteEdiRol/src/main/kotlin/com/example/siteEdiRol/Controller.kt
@@ -1,4 +1,12 @@
 package com.example.siteEdiRol
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/home")
 class Controller {
+    @GetMapping("/teste")
+    fun sum(n1: Int, n2: Int) = n1 + n2
 }
